493c0fe3c91ce52d14b9cd31759da1d95f04eb31
Raphaël Masse <masseraphael76@gmail.com>
2021-02-14 17:20:07 +0100

    contrôle de la raquette et potentiel solution pour le game over

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,10 @@ import Phaser from '../lib/phaser.js';
 
 export default class Game extends Phaser.Scene
 {
+    paddle;
+    cursors;
+    bricks;
+
     constructor()
     {
         super('game')
@@ -13,19 +17,32 @@ export default class Game extends Phaser.Scene
         this.load.image('brick', 'assets/tileRed_26.png');
         this.load.image('ball', 'assets/ballYellow_06.png');
         this.load.image('paddle', 'assets/paddle_02.png');
+        this.load.image('ground', 'assets/ground.png');
+
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     create()
     {
         this.add.image(500, 400, 'background');
 
-        this.add.image(500, 400, 'ball').setScale(0.1);
+        //const ground = this.physics.add.staticImage(500, 800, 'ground');
 
-        const paddle = this.physics.add.staticImage(500, 770, 'paddle').setScale(0.15).body.updateFromGameObject();
+        this.paddle = this.physics.add.image(500, 770, 'paddle').setScale(0.15);
+        this.paddle.setCollideWorldBounds(true);
+
+        //this.physics.add.collider(this.paddle, ground);
     }
 
     update()
     {
-
+        if(this.cursors.left.isDown)
+        {
+            this.paddle.setVelocityX(-200);
+        }
+        else if(this.cursors.right.isDown)
+        {
+            this.paddle.setVelocityX(200);
+        }
     }
 }
