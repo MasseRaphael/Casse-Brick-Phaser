62c00b7c19ebc95eb0b6d4691498cd75c224f756
Raphaël Masse <masseraphael76@gmail.com>
2021-02-15 15:39:18 +0100

    ajout d'un deuxième niveau en cours d'élaboration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Phaser from './lib/phaser.js';
 import Level_1 from './scenes/Level_1.js';
+import Level_2 from './scenes/Level_2.js';
 import GameOver from './scenes/GameOver.js';
 import StartGame from './scenes/StartGame.js';
 
@@ -7,7 +8,7 @@ export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 1000,
     height: 600,
-    scene: [StartGame, Level_1, GameOver],
+    scene: [StartGame, Level_2, GameOver],
     physics: {
         default: 'arcade',
         arcade: {
---
+++ src/scenes/Level_2.js
@@ -0,0 +1,156 @@
+import Phaser from '../lib/phaser.js';
+
+export default class Level_2 extends Phaser.Scene
+{
+    //Déclaration des variables utiliséé
+    paddle;
+    cursors;
+    bricks;
+    ball;
+    score;
+    brickLife;
+
+    constructor()
+    {
+        super('level_1')
+    }
+
+    //initialisation du score
+    init()
+    {
+        this.score = 0;
+    }
+
+    //préchargement des images utilisées pour le niveau
+    preload()
+    {
+        this.load.image('background', 'assets/bg_layer1.png');
+        this.load.image('brick1', 'assets/tileRed_26.png');
+        this.load.image('brick2', 'assets/tileRed_26.png');
+        this.load.image('brick3', 'assets/tileRed_38.png');
+        this.load.image('ball', 'assets/ballYellow_06.png');
+        this.load.image('paddle', 'assets/paddle_02.png');
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    //Génération du niveau
+    create()
+    {
+        this.add.image(500, 400, 'background');
+
+        //génération du mur de briques---------------------
+        this.bricks = this.physics.add.staticGroup();
+
+        for(let i = 1; i < 76; ++i)
+        {
+            const x = i * 60;
+            
+            if( x <= 950)
+            {
+                const brick = this.bricks.create(x, 30, 'brick1').setScale(0.3);
+                brick.body.updateFromGameObject();
+            }
+            else if( x >= 950 && x <= 1850)
+            {
+                const x = ( i - 15 ) * 60 
+                const brick = this.bricks.create(x, 60, 'brick1').setScale(0.3);
+                brick.body.updateFromGameObject();
+            }
+            else if( x >= 1850 && x <= 2750)
+            {
+                const x = ( i - 30 ) * 60 
+                const brick = this.bricks.create(x, 90, 'brick1').setScale(0.3);
+                brick.body.updateFromGameObject();
+            }
+            else if( x >= 2750 && x <= 3650)
+            {
+                const x = ( i - 45 ) * 60 
+                const brick = this.bricks.create(x, 120, 'brick1').setScale(0.3);
+                brick.body.updateFromGameObject();
+            }
+            else if( x >= 3650 && x <= 4550)
+            {
+                const x = ( i - 60 ) * 60 
+                const brick = this.bricks.create(x, 150, 'brick1').setScale(0.3);
+                brick.body.updateFromGameObject();
+            }
+        }
+        //-------------------------------------------------
+
+        //génération de la raquette------------------------
+        this.paddle = this.physics.add.image(500, 570, 'paddle').setScale(0.15);
+        //permet d'empêcher la raquette de sortir de l'écran
+        this.paddle.setCollideWorldBounds(true);
+        //-------------------------------------------------
+
+        //génération de la balle---------------------------
+        this.ball = this.physics.add.image(500, 300, 'ball').setScale(0.1);
+        this.ball.setBounce(1);
+        //permet d'empêcher la balle de sortir de l'écran
+        this.ball.setCollideWorldBounds(true);
+        //-------------------------------------------------
+
+        //paramétrage des collisons------------------------
+        this.physics.add.collider(this.ball, this.paddle);
+
+        this.physics.add.collider(
+            this.ball,
+            this.bricks,
+            this.hitBrick,
+            undefined,
+            this
+            );
+        //-------------------------------------------------
+
+        const style = { fontSize: 32, color: '#000'};
+        this.scoreText = this.add.text(400, 300, 'Score: 0', style);
+    }
+
+    //Eléments mis à jour selon certaines conditions
+    update()
+    {
+        //paramétrage du rebond de la balle----------------
+        const touchingDown = this.ball.body.touching.down;
+
+        if(touchingDown)
+        {
+            //paramétrage du rebond avec un paramètre aléatoire sur l'axe x pour éviter le soft-bloc avec un vélocité sur x égale à 0
+            this.ball.setVelocity(Phaser.Math.Between(-200, 200), -500)
+        }
+        //-------------------------------------------------
+
+        //paramétrage du movement de la raquette-----------
+        if(this.cursors.left.isDown)
+        {
+            this.paddle.setVelocityX(-300);
+        }
+        else if(this.cursors.right.isDown)
+        {
+            this.paddle.setVelocityX(300);
+        }
+        else
+        {
+            this.paddle.setVelocityX(0);
+        }
+        //-------------------------------------------------
+
+        //paramétrage du Game Over-------------------------
+        if(this.ball.y > 592)
+        {
+            this.scene.start('game-over', {score: this.score});
+        }
+        //-------------------------------------------------
+    }
+
+    //Fonction d'impact de la balle contre les briques permettant de détruire et ajouter des points au score
+    hitBrick(ball, brick)
+    {
+        brick.destroy();
+
+        this.score += 10;
+
+        const value = `Score: ${this.score}`;
+        this.scoreText.text = value;
+    }
+}
